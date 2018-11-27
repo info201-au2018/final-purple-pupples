8061e244e26a81600f68ced919d35ca5543fb70c
CJ Hillbrand <cjhillbrand@gmail.com>
2018-11-27 10:54:30 -0800

    Edit ReadMe

--- twitter_api.R
+++ twitter_api.R
@@ -2,13 +2,21 @@ users <- read.delim("./users.csv", stringsAsFactors = FALSE, sep = ";")
 media <- read.delim("./media.csv", stringsAsFactors = FALSE, sep =";")
 View(users)
 
-library(twitteR)
+library(rtweet)
+source("keys.R")
+token <- create_token(app ="R Shiny analytics project", consumer_key_val, consumer_secret_val)
 
-setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 
-for (i in 1:10) {
-  tw = twitteR::searchTwitter('#maga', n = 1000, since = '2000-11-08', retryOnRateLimit = 1e3)
-  d = twitteR::twListToDF(tw)
-  print(d$text)  
-}
+tw = twitteR::searchTwitter('#maga', n = 1000, since = '2000-11-08', retryOnRateLimit = 1e3)
+d = twitteR::twListToDF(tw)
+print(d$text)  
 View(d)
+
+result <- twitteR::userTimeline('realDonaldTrump')
+result$location
+user_search <- twitteR::searchTwitter(result$screenName, n = 25)
+user_search
+result[1:10]
+
+
+rt <- twitteR::searchTwitter()
