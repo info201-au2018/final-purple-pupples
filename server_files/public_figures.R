5d00890358eeb58316c5a03c84322877eab66233
CJ Hillbrand <cjhillbrand@gmail.com>
2018-11-27 14:20:58 -0800

    Isolated oauth

--- server_files/public_figures.R
+++ server_files/public_figures.R
@@ -1,9 +1,5 @@
 library(twitteR)
 <<<<<<< Updated upstream
-source("keys.R")
-setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
-
-
 tw = twitteR::searchTwitter('#maga', n = 1000, since = '2000-11-08', retryOnRateLimit = 1e3)
 d = twitteR::twListToDF(tw)
 print(d$text)  
