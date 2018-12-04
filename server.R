f42c05d10ea03169cd9791535bb1cf36d57cbd30
Ninaslazinik <nina.slazinik@gmail.com>
2018-12-03 21:07:37 -0800

    ninas up to date

--- server.R
+++ server.R
@@ -1,51 +1,72 @@
 source("./server_files/tv_shows_server.R")
 source("./server_files/public_figures.R")
 
-my_server <- function(input, output){
-getTable <- reactive({
-  table <- kardashian_tweets
-  if(input$text != "") {
-  table <- get_tweets(input$text)
-  table <- get_top_tweets(table)
- }
-  table
-})
-  output$TV_times <- renderPlot({
-    plots
-  })
-  data <- reactive ({
-    bar <- bar_plot
-    if (input$radio2 == "computer") {
-     if (input$radio == "Retweets") {
-       bar <- bar_plot2
-     } else {
-       bar <- bar_plot3
-     }
+my_server <- function(input, output) {
+  getTable <- reactive({
+    table <- kardashian_tweets
+    if(input$text != "") {
+      table <- get_tweets(input$text)
+      table <- get_top_tweets(table)
     }
-    if (input$radio2 == "phone/tablet") {
-      if (input$radio == "Retweets") {
-        bar <- bar_plot2
-      } else {
-      bar <- bar_plot4
+    table
+  })
+  get_tweets <- reactive ({
+    tweets_graph <- plots
+    if(input$select2 == "Computer") {
+      tweets_graph <- plots_comp
+    }
+    if(input$select2 == "Phone/Ipad") {
+      tweets_graph <- plots_dev
+    }
+    tweets_graph
+  })
+  getPlot <- reactive ({
+    plot <- gg
+    if(input$select == "Retweets") {
+      if(input$tvshow == "Keeping up with the Kardashians") {
+        plot <- retweets_plot_kardash
+      } 
+      if (input$tvshow == "American Horror Story") {
+        plot <- retweets_plot_ahs
+      }
+      if (input$tvshow == "The Voice") {
+        plot <- retweets_plot_voice
+      }
+      if (input$tvshow == "Saturday Night Live") {
+        plot <- retweets_plot_snl
       }
     }
-    bar
-})
-  output$favorites <- renderPlot({
-    data()
+     if(input$select == "Favorites") {
+        plot <- favorites_plot
+        if(input$tvshow == "Keeping up with the Kardashians") {
+          plot <- favs_plot_kardash
+        } 
+        if (input$tvshow == "American Horror Story") {
+          plot <- favs_plot_ahs
+        }
+        if (input$tvshow == "The Voice") {
+          plot <- favs_plot_voice
+        }
+        if (input$tvshow == "Saturday Night Live") {
+          plot <- favs_plot_snl
+        }
+    }
+    plot
+  })
+  output$TV_times <- renderPlot({
+    get_tweets()
+  })
+  output$fav <- renderPlot({
+    getPlot()
   })
   output$tweetTable <- renderDataTable({getTable()})
-
-
   output$word_cloud_text <- renderText(
     word_cloud_message(input)
   )
   output$public_figure_word_cloud <- renderPlot({
     word_cloud(input)
   }, width = 800, height = 600)
-
+  
   
   
 }
-
-shinyServer(my_server)
